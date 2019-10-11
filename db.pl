f5ffd29a4fee64fc13e24558e4b807edb3037b0e
Clarissa Borges <clarissalimab08@gmail.com>
2019-10-11 00:33:33 -0300

    Add initial physical characteristics and chacater

---
+++ db.pl
@@ -0,0 +1,160 @@
+% clarissa
+% felipe
+% mariana
+% romeu
+% maurício
+% milene
+% carla
+% renato
+% fábio
+% edson
+% cacau
+% arya
+% naveen
+% meg
+% koda
+% nina
+
+% olho(nome, cor)
+olho(clarissa, azul).
+olho(bruna, castanho).
+olho(felipe, castanho).
+olho(mariana, castanho).
+olho(romeu, castanho).
+olho(mauricio, castanho).
+olho(milene, castanho).
+olho(carla, azul).
+olho(renato, verde).
+olho(fabio, verde).
+olho(edson, castanho).
+olho(cacau, castanho).
+olho(arya, azul).
+olho(naveen, amarelo).
+olho(meg, castanho).
+olho(koda, castanho).
+olho(nina, castanho).
+
+% cabelo(nome, cor) / cabelo(nome, tamanho)
+cabelo(clarissa, preto).
+cabelo(clarissa, curto).
+
+cabelo(bruna, castanho).
+cabelo(bruna, longo).
+
+cabelo(felipe, preto).
+cabelo(felipe, curto).
+
+cabelo(mariana, preto).
+cabelo(mariana, longo).
+
+cabelo(romeu, preto).
+cabelo(romeu, curto).
+
+cabelo(mauricio, castanho).
+cabelo(mauricio, curto).
+
+cabelo(milene, castanho).
+cabelo(milene, longo).
+
+cabelo(carla, loiro).
+cabelo(carla, curto).
+
+cabelo(renato, loiro).
+cabelo(renato, curto).
+
+cabelo(fabio, grisalho).
+cabelo(fabio, curto).
+
+cabelo(edson, preto).
+cabelo(edson, curto).
+
+pelo(cacau, beige).
+pelo(cacau, curto).
+
+pelo(arya, mesclado).
+pelo(arya, longo).
+
+pelo(naveen, amarelo).
+pelo(naveen, curto).
+
+pelo(meg, marrom).
+pelo(meg, curto).
+
+pelo(koda, mesclado).
+pelo(koda, curto).
+
+pelo(nina, marrom).
+pelo(nina, curto).
+
+% especie(nome, especie)
+especie(clarissa, humano).
+especie(bruna, humano).
+especie(felipe, humano).
+especie(mariana, humano).
+especie(romeu, humano).
+especie(mauricio, humano).
+especie(milene, humano).
+especie(carla, humano).
+especie(renato, humano).
+especie(fabio, humano).
+especie(edson, humano).
+especie(cacau, cachorro).
+especie(arya, gato).
+especie(naveen, gato).
+especie(meg, cachorro).
+especie(koda, cachorro).
+especie(nina, cachorro).
+
+% genero(nome, genero)
+genero(clarissa, feminino).
+genero(bruna, feminino).
+genero(felipe, masculino).
+genero(mariana, feminino).
+genero(romeu, masculino).
+genero(mauricio, masculino).
+genero(milene, feminino).
+genero(carla, feminino).
+genero(renato, masculino).
+genero(fabio, masculino).
+genero(edson, masculino).
+genero(cacau, feminino).
+genero(arya, feminino).
+genero(naveen, masculino).
+genero(meg, feminino).
+genero(koda, masculino).
+genero(nina, feminino).
+
+% alunos
+aluno(clarissa).
+aluno(bruna).
+aluno(felipe).
+aluno(mariana).
+aluno(romeu).
+
+% professores
+professor(mauricio).
+professor(milene).
+professor(carla).
+professor(renato).
+professor(fabio).
+professor(edson).
+
+% barba
+barba(renato).
+barba(fabio).
+barba(edson).
+barba(felipe).
+barba(romeu).
+
+% oculos
+oculos(felipe).
+oculos(bruna).
+oculos(renato).
+oculos(fabio).
+oculos(edson).
+
+
+
+
+
+
